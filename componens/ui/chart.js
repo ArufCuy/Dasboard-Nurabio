cc7c5a785e6e6c64caf9a5c6be864e28c65a7f4c
ArufCuy <77611639+ArufCuy@users.noreply.github.com>
2025-07-26 11:34:47 +0800

    Add files via upload

---
+++ componens/ui/chart.js
@@ -0,0 +1,8 @@
+// components/ui/chart.js
+export const Chart = () => null
+export const ChartContainer = () => null
+export const ChartTooltip = () => null
+export const ChartTooltipContent = () => null
+export const ChartLegend = () => null
+export const ChartLegendContent = () => null
+export const ChartStyle = () => null
