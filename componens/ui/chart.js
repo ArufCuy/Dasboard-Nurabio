2b1ad3259f46f09efbca2ff4ef0b3b089c618819
ArufCuy <77611639+ArufCuy@users.noreply.github.com>
2025-07-27 10:57:06 +0800

    Delete componens directory

--- componens/ui/chart.js
+++
@@ -1,8 +0,0 @@
-// components/ui/chart.js
-export const Chart = () => null
-export const ChartContainer = () => null
-export const ChartTooltip = () => null
-export const ChartTooltipContent = () => null
-export const ChartLegend = () => null
-export const ChartLegendContent = () => null
-export const ChartStyle = () => null
